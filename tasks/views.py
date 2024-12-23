de15794baccda0d0d800c3e73b8860746c6353e4
Tomás R. <tomaslopezraigal@gmail.com>
2024-12-24 00:27:23 +0100

    Edit task

--- tasks/views.py
+++ tasks/views.py
@@ -1,12 +1,10 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Task
 
-# Mostrar todas las tareas
 def task_list(request):
     tasks = Task.objects.all().order_by('priority')
     return render(request, 'tasks/task_list.html', {'tasks': tasks})
 
-# Añadir una nueva tarea
 def add_task(request):
     if request.method == 'POST':
         title = request.POST['title']
@@ -15,21 +13,28 @@ def add_task(request):
         return redirect('task_list')
     return render(request, 'tasks/add_task.html')
 
-# Marcar una tarea como completada
 def complete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.completed = True
     task.save()
     return redirect('task_list')
 
-# Eliminar una tarea
 def delete_task(request, task_id):
     task = Task.objects.get(id=task_id)
     task.delete()
     return redirect('task_list')
 
+def edit_task(request, task_id):
+    task = Task.objects.get(id=task_id)
+    if request.method == 'POST':
+        task.title = request.POST['title']
+        task.priority = request.POST['priority']
+        task.save()
+        return redirect('task_list')
+    return render(request, 'tasks/edit_task.html', {'task': task})
+
 def task_toggle(request, task_id):
     task = get_object_or_404(Task, id=task_id)
-    task.completed = not task.completed  # Cambiar el estado de 'completed'
+    task.completed = not task.completed
     task.save()
     return redirect('task_list')
--- todo_project/urls.py
+++ todo_project/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('add/', views.add_task, name='add_task'),
     path('complete/<int:task_id>/', views.complete_task, name='complete_task'),
     path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-    path('task/<int:task_id>/toggle/', views.task_toggle, name='task_toggle'),  # URL para marcar como completada/no completada
+    path('edit/<int:task_id>/', views.edit_task, name='edit_task'),
+    path('task/<int:task_id>/toggle/', views.task_toggle, name='task_toggle'),
 ]
